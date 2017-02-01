ba01bb5e591c64182d4119b5814305e5349dcf19
StavSheiz <stav54655@gmail.com>
2017-02-01 20:54:01 +0200

    game slider working - need design

--- Client/components/app/pick/Pick.jsx
+++ Client/components/app/pick/Pick.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import PickGame from './pickGame/PickGame.jsx'
 //import PickTeam from './pickTeam/PickTeam.jsx'
+
+var games = [{id:"hi", index: 1},{id: "gi", index:2},{id: "bi", index: 3},{id:"fi", index:4}, {id:"jo", index:5}]
 class Pick extends React.Component{
 
     constructor(props){
@@ -16,11 +18,13 @@ class Pick extends React.Component{
                 <img src="image"/>
                 {
                     //(this.state.page == "GAME")?
-                    (<PickGame/>)//:
+                    (<PickGame onGamePick={this.onGamePick} games={games}/>)//:
                     //(<PickTeam/>)
                 }
             </div>);
     }
+
+    onGamePick(){}
 }
 
 export default Pick;    
